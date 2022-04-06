ed1b913e7f70b7c3598726d358de34f191c1211a
srb <srb23@njit.edu>
2022-04-06 13:11:10 -0400

    tests

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+from app.auth.decorators import admin_required
 from flask_login import login_user, login_required, logout_user, current_user
 from sqlalchemy.orm import load_only
 from werkzeug.security import generate_password_hash
 from app.auth.forms import login_form, register_form, profile_form, security_form, user_edit_form
 from app.db import db
 from app.db.models import User
-from app.auth.decorators import admin_required
+
 auth = Blueprint('auth', __name__, template_folder='templates')
 from flask import current_app
 
@@ -167,4 +169,5 @@ def edit_account():
         flash('You Successfully Updated your Password or Email', 'success')
         return redirect(url_for('auth.dashboard'))
     return render_template('manage_account.html', form=form)
+
 
