4ab15fcd3c3a5c7461a6860c6dfc87f75aade826
srb <srb23@njit.edu>
2022-04-06 12:47:34 -0400

    tests

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-database = Blueprint('database', __name__,)
+database = Blueprint('database', __name__, )
+
 
 @database.cli.command('create')
 def init_db():
